54a8b8826ceb24eef2049cf8bb14303dd173f20e
abdoduude <abdoduude@yahoo.com>
2020-03-13 22:17:17 +0100

    + ()

---
+++ app/src/main/java/de/unlimitedstudios/coronacommunicator/AppObject.java
@@ -0,0 +1,11 @@
+package de.unlimitedstudios.coronacommunicator;
+
+import android.app.Application;
+
+public class AppObject extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
